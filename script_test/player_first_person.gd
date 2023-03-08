373fdd9e7576212fedda3c7c7c5150f2e3d330b3
ARez <dark.gaming@fantasymail.de>
2023-03-08 22:21:34 +0100

    Improve raycast stepping

--- script_test/player_first_person.gd
+++ script_test/player_first_person.gd
@@ -4,31 +4,31 @@
 # meta-space-indent: 4
 extends CharacterBody3D
 
+@export var camera : Camera3D
+@export_group("Movement")
 @export var max_speed: float = 5.0 			# Geschwindigkeit des Spielers
 @export var accel: float = 0.5 			# Beschleunigung des Spielers
 @export var deaccel: float = 1.0 			# Abbremsen des Spielers
 @export var jump_velocity: float = 4.5	# Sprunghöhe
+@export_group("Mouse")
 @export var mouse_sensivity: float =  0.25	# Geschwindigkeit der Mausdrehung
 @export var invert_mouse_y := false # Y-Achse invertieren
 @export var invert_mouse_x := false # X-Achse invertieren
+@export_group("Step")
 @export var step_height := 0.25 # Wie hoch sind die stufen die bestiegen werden sollen
-@export var step_raylength := 0.5 # Wie weit voraus soll nach stufen geschaut werden
-#@export var gravity_vector :Vector3 = Vector3(0.0, -1.0, 0.0)	# Richtung der Gravitation
-
-@export var camera : Camera3D
+@export var step_raylength := 0.3 # Wie weit voraus soll nach stufen geschaut werden
+@export var num_step_ray_directions := 4
 
 var mouse_captured := false	# Merkt sich ob die Maus gefangen ist
 const min_camRotX := -PI/2	# kleinster Winkel in radians bis zu dem man nach unten sehen kann
 const max_camRotX := PI/2	# größter Winkel in radians bis zu dem man nach oben sehen kann
 var direction := Vector3.ZERO	# Tasten Bewegungs Richtung
 var input_vector := Vector2.ZERO # Eingabe vektor
-
+var bottom_step_rays : Array[RayCast3D] = []
+var top_step_rays : Array[RayCast3D] = []
 
 # Gravitation von den Projekt Einstellungen laden damit diese mit den RigidBody Nodes synchron ist.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
-@onready var bottom_step_ray := RayCast3D.new() # unterer stufen ray
-@onready var top_step_ray := RayCast3D.new() # oberer stufen ray
 
 
 # Tastatur Eingabe Festlegen
@@ -69,17 +69,35 @@ func _set_inputs():
 		InputMap.action_add_event("switchmode", new_switchmode)
 
 
-func _setup_step_rays():
-	# Setze den unteren ray knapp über den boden
-	bottom_step_ray.position.y += 0.01
-	top_step_ray.position.y = bottom_step_ray.position.y + step_height
-	
-	# Lasse beide Rays in die selbe richtung schauen
-	bottom_step_ray.target_position = Vector3(0, 0, -step_raylength)
-	top_step_ray.target_position = Vector3(0, 0, -step_raylength)
-	
-	add_child(bottom_step_ray)
-	add_child(top_step_ray)
+func _setup_step_rays() -> void:
+	var step := deg_to_rad(360) / num_step_ray_directions
+	for i in range(num_step_ray_directions):
+		var bottom_step_ray := RayCast3D.new()
+		# Setze den unteren ray knapp über den boden
+		bottom_step_ray.position.y += 0.01
+		
+		var top_step_ray := RayCast3D.new()
+		top_step_ray.position.y = bottom_step_ray.position.y + step_height
+		
+		# Lasse beide Rays in die selbe richtung schauen
+		bottom_step_ray.target_position = Vector3(0, 0, -step_raylength).rotated(Vector3.UP, i * step)
+		top_step_ray.target_position = Vector3(0, 0, -step_raylength).rotated(Vector3.UP, i * step)
+		
+		bottom_step_rays.append(bottom_step_ray)
+		top_step_rays.append(top_step_ray)
+		add_child(bottom_step_ray)
+		add_child(top_step_ray)
+
+
+
+func check_ray_collision() -> bool:
+	#	if input_vector.y > 0 and bottom_step_ray.is_colliding() and bottom_step_ray.get_collision_normal().y == 0 and !top_step_ray.is_colliding():
+	for i in range(bottom_step_rays.size()):
+		var bottom_step_ray := bottom_step_rays[i]
+		var top_step_ray := top_step_rays[i]
+		if bottom_step_ray.is_colliding() and bottom_step_ray.get_collision_normal().y == 0 and !top_step_ray.is_colliding():
+			return true
+	return false
 
 
 # Beim Start ausführen
@@ -101,7 +119,7 @@ func _ready() -> void:
 # Eingaben prüfen
 func _input(event):
 	# Wenn Maus Bewegung (Umschauen)
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion and mouse_captured:
 		var x_multiplier = -1 if invert_mouse_x else 1
 		var y_multiplier = -1 if invert_mouse_y else 1
 		rotate_y(-event.relative.x * 0.01 * mouse_sensivity * x_multiplier)
@@ -125,7 +143,7 @@ func _process(delta) -> void:
 
 
 func _physics_process(delta) -> void:
-	if input_vector.y > 0 and bottom_step_ray.is_colliding() and !top_step_ray.is_colliding():
+	if input_vector != Vector2.ZERO and check_ray_collision():
 		# kraft, die nur etwas größer als schwerkraft ist um spieler anzuheben
 		velocity.y += gravity * delta + 0.1
 	
